0153a46f6ce73670074122c64f98fa8e1c72393c
River Wang <urfdvw@gmail.com>
2023-11-08 18:56:30 -0500

    working example

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
-import DarkTheme from "./react-lazy-dark-theme";
+import DarkTheme from "react-lazy-dark-theme";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [dark, setDark] = useState(null);
+    console.log(dark);
 
     return (
         <>
-            <DarkTheme />
+            <DarkTheme dark={dark} />
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -20,7 +21,25 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <select
+                    onChange={(e) => {
+                        switch (e.target.value) {
+                            case "Dark":
+                                setDark(true);
+                                break;
+                            case "Light":
+                                setDark(false);
+                                break;
+                            case "System":
+                                setDark(null);
+                                break;
+                        }
+                    }}
+                >
+                    <option>System</option>
+                    <option>Dark</option>
+                    <option>Light</option>
+                </select>
                 <p>
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+// https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        lib: {
-            entry: path.resolve("src", "react-lazy-dark-theme/index.jsx"),
-            name: "react-lazy-dark-theme",
-            fileName: (format) => `react-lazy-dark-theme.${format}.js`,
-        },
-        rollupOptions: {
-            external: ["react", "react-dom"],
-            output: {
-                globals: {
-                    react: "React",
-                },
-            },
-        },
-        outDir: "./release",
-    },
     plugins: [react()],
+    base: "./",
 });
