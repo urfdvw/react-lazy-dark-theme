264a0072f230616ba674854fc5ac3cec01947390
River Wang <urfdvw@gmail.com>
2025-01-23 11:46:54 -0500

    demo up to version 0.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,62 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
-import DarkTheme from "react-lazy-dark-theme";
+import DarkTheme, { NoTheme } from "react-lazy-dark-theme";
+import { useEffect } from "react";
 
-const options = {
+const darkOptions = {
     Dark: true,
     Light: false,
     System: null,
 };
 
+const contrastOptions = {
+    "Night Mode": false,
+    "High Contrast": true,
+};
+
 function App() {
     const [dark, setDark] = useState(null);
+    const [highContrast, setHighContrast] = useState(false);
+    useEffect(() => {
+        console.log("dark,highContrast", [dark, highContrast]);
+    }, [dark, highContrast]);
     console.log(dark);
 
     return (
         <>
-            <DarkTheme dark={dark} />
+            <DarkTheme dark={dark} highContrast={highContrast} />
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+                    <NoTheme>
+                        <img src={viteLogo} className="logo" alt="Vite logo" />
+                    </NoTheme>
                 </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                    <NoTheme>
+                        <img src={reactLogo} className="logo react" alt="React logo" />
+                    </NoTheme>
                 </a>
             </div>
             <h1>Vite + React</h1>
             <div className="card">
                 <select
                     onChange={(e) => {
-                        setDark(options[e.target.value]);
+                        setDark(darkOptions[e.target.value]);
                     }}
                 >
                     <option>System</option>
                     <option>Dark</option>
                     <option>Light</option>
+                </select>
+
+                <select
+                    onChange={(e) => {
+                        setHighContrast(contrastOptions[e.target.value]);
+                    }}
+                >
+                    <option>Night Mode</option>
+                    <option>High Contrast</option>
                 </select>
                 <p>react-lazy-dark-theme demo.</p>
             </div>
